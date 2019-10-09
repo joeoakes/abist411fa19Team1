a611355e9061e730c8b8f7fb438b491fa9477686
David Chen <dkc5318@psu.edu>
2019-10-09 22:07:31 +0000

    mkdir for App1

---
+++ App1/app1Main.py
@@ -0,0 +1,20 @@
+#Project: Project Diamond
+#Purpose Details: Retrieve a JSON payload from the internet and send it to App2 using TLS. It will also retrieve an encrypted payload from App4 and save the JSON payload to a text file.
+#Course: IST 411
+#Author: Team 1
+#Date Developed: 10/9/2019
+#Last Date Changed: 
+#Rev: 0
+
+
+#This will call all other classes created, related to App1.
+
+
+
+
+
+def main():
+    print("Not Finished")
+
+if __name__ == '__main__':
+    main()
