a308448277f465255ab7e4781bce9f934be81810
ErikEllis <eae5206@psu.edu>
2019-10-11 20:09:26 +0000

    Added test class

---
+++ App1/TestSendPayload.py
@@ -0,0 +1,15 @@
+# Project: Project Diamond
+# Purpose Details: Test of SendPayload
+# Course: IST 411
+# Author: Erik Ellis
+# Date Developed: 10/11/19
+# Last Date Changed:
+# Rev:
+import unittest
+from SendPayload import sendPayload
+
+class TestSendPayload(unittest.TestCase):
+
+   # Testing  sendPayload method
+   def testSendPayload(self, payload):
+      
