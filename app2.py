1197c8258281a8ed00efe1622b7d42e680bbb658
teresabarker <tlb5767@psu.edu>
2019-10-16 21:33:28 +0000

    message

--- app2.py
+++ app2.py
@@ -3,8 +3,8 @@
 # Course: IST 411-001
 # Author: Team 1, Teresa Barker and Asraa Alkurdi
 # Date Developed: October 9, 2019
-# Last Date Revised: October 13, 2019
-# Rev: 2
+# Last Date Revised: October 16, 2019
+# Rev: 4
 
 import socket
 import ssl
@@ -13,12 +13,15 @@ try:
     # Receive the secure payload using TLS
     print("App 2 connecting on port 8080 using SSL (TLS)")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ssl_sock = ssl.wrap_socket(s, ca_certs="server.crt", cert_reqs=ssl.CERT_REQUIRED)
-    ssl_sock.connect(('localhost', 8080))
+    ssl_socket = ssl.wrap_socket(s, ca_certs="team1tls.crt", cert_reqs=ssl.CERT_REQUIRED)
+
+    # Listen on port 8080
+    ssl_socket.bind((socket.gethostname(), 8080))
+    ssl_socket.listen(5)
 
     while True:
         print("Accept connections from outside")
-        (clientSocket, address) = ssl_sock.accept()
+        (clientSocket, address) = ssl_socket.accept()
         print(clientSocket.recv(1024))
 
     # Log pass/fail workflow actions into the MongoDB database with timestamp
