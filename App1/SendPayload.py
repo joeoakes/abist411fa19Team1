f19ffca418e9c9091489ddd434c1ddb9b12a67bf
David Chen <dkc5318@psu.edu>
2019-10-17 00:44:22 +0000

    fixed more buggs

--- App1/SendPayload.py
+++ App1/SendPayload.py
@@ -13,29 +13,31 @@ import socket, ssl, json, datetime
 class SendPayload:
 
    # Connect to server and send payload
-   def sendPayload(self, payload):
-      try:
-         print("App 1 connecting on port 8080 using TLS")
-         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-         ssl_sock = ssl.wrap_socket(s,
-            ca_certs="team1tls.crt",
-            cert_reqs=ssl.CERT_REQUIRED)
-         ssl_sock.connect(('localhost',8080))
-         ssl_sock.sendall(json.dumps(payload).encode())
-         print("JSON payload sent to _______ using TLS")
-         ssl_sock.close()
-         print(ssl_sock.cipher())
-         # Logging
+    def sendPayload(self, payload):
+        try:
+            print("App 1 connecting on port 8080 using TLS")
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            ssl_sock = ssl.wrap_socket(s,
+                ca_certs="team1tls.crt",
+                cert_reqs=ssl.CERT_REQUIRED)
+            ssl_sock.connect(('localhost',8080))
+            #return True
+            ssl_sock.sendall(json.dumps(payload).encode())
+            print("JSON payload sent to _______ using TLS")
+            ssl_sock.close()
+            return True
+ #        print(ssl_sock.cipher())
+        # Logging
  #        MongoDB.mongoInstance("Test","Sent to app2")
-         return True
+            return True
 
-      except Exception as e:
+        except Exception as e:
     #     client = MongoClient('localhost', 27017)
      #    db = client.Team1
      #    collection = db.logs
-
-         print(e)
-
+            
+            print(e)
+            return False
          #Logging
   #       MongoDB.mongoInstance("Test","Failed to send to app2")
          #return False
--- App1/TestSendPayload.py
+++ App1/TestSendPayload.py
@@ -7,21 +7,10 @@
 # Rev: 2
 import unittest, socket, ssl
 from SendPayload import SendPayload
-
+from PayloadRetriever import PayloadRetriever
 class TestSendPayload(unittest.TestCase):
 
-   #def setUp(self):
-   #     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   #     serverssl_socket = ssl.wrap_socket(s, server_side=True, certfile="team1tls.crt", keyfile="team1tls.key")
-   #     serverssl_socket.bind(('localhost', 8080))
-   #     serverssl_socket.listen(5)
-   #     while True:
-   #          client, address = serverssl_socket.accept()
-
-   #def tearDown(self):
-   #     serverssl_socket.close()
-
    def testSendPayload(self):
-        payload = "message"
-        self.assertEqual(SendPayload().sendPayload(payload), True)
+        results = SendPayload().sendPayload(PayloadRetriever().readAndDecodeJSON())
+        self.assertTrue(results)
         
