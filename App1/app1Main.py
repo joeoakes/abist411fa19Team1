5b61cc98e4359e2545d65f73e853523899ab3344
ErikEllis <eae5206@psu.edu>
2019-11-04 20:07:01 +0000

    Edited datetime, added App3 and started app3Main.py

--- App1/app1Main.py
+++ App1/app1Main.py
@@ -1,30 +1,29 @@
-
 #Project: Project Diamond
 #Purpose Details: Retrieve a JSON payload from the internet and send it to App2 using TLS. It will also retrieve an encrypted payload from App4 and save the JSON payload to a text file.
 #Course: IST 411
 #Author: Team 1
 #Date Developed: 10/9/2019
-#Last Date Changed:10/12/2019
+#Last Date Changed:11/04/2019
 #Rev: 0
 
 
 #This will call all other classes created, related to App1.
-import sys
+import sys, datetime
 from PayloadRetriever import PayloadRetriever
 from PayloadSaver import PayloadSaver
 from SendPayload import SendPayload
 from mongo import MongoDB
-from datetime import datetime
-
 
 def main():
 
-   timestamp = 1545730073
-   dtObject = datetime.fromtimestamp(timestamp)
+    timestamp = 1545730073
+    startDtObject = datetime.fromtimestamp(timestamp)
 #    client = MongoClient('localhost', 27017)
 #    db = client.Team1
 #    collection = db.logs
     mongoDB = MongoDB()
+
+    print(startDtObject)
     print("Retrieving JSON payload from source.")
     payload = PayloadRetriever().readAndDecodeJSON(mongoDB)
 
@@ -34,10 +33,9 @@ def main():
     print("Saving payload to text file.")
     PayloadSaver().savePayload(payload,mongoDB)
 
+    endDtObject = datetime.fromtimestamp(timestamp)
 
 
 if __name__ == '__main__':
     main()
-
-
 
---
+++ App3/app3Main.py
@@ -0,0 +1,17 @@
+# Project: Project Diamond App3
+# Purpose Details: Secure file transfer with SFTP
+# Course: IST 411 Fall 2019
+# Author: Erik Ellis
+# Date Developed: 11/04/19
+# Last Date Changed:
+# Rev: 0
+
+import sys, datetime
+
+def main():
+
+    timestamp = 1545730073
+    startDtObject = datetime.fromtimestamp(timestamp)
+
+
+    endDtObject = datetime.fromtimestamp(timestamp)
